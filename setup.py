5d8333ca8cd81571c271901b549a86df66c11230
Martin Durant <martin.durant@utoronto.ca>
2017-12-14 11:52:52 -0500

    Set up package

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+
+setup(name='dask-adlfs',
+      version='0.0.1',
+      description='Parallel PyData with Task Scheduling',
+      url='https://github.com/dask/dask-adlfs/',
+      maintainer='Martin Durant',
+      maintainer_email='martin.durant@utoronto.ca',
+      license='BSD',
+      keywords=['file-system', 'dask', 'azure'],
+      packages=['dask_adlfs'],
+      long_description=open('README.rst').read() if exists('README.rst') else '',
+      install_requires=['azure-datalake-store', 'dask'],
+      zip_safe=False)
