ed0edaec08348269608df05cfb386ebea8c8cfd6
Greg Hayes <hayesgb@gmail.com>
2019-08-13 20:10:50 -0500

    core.py ls method started

--- adlfs/core.py
+++ adlfs/core.py
@@ -112,11 +112,11 @@ class AzureBlobFileSystem(AbstractFileSystem):
     account_name = The name of the storage account
     path         =  A forward slash representation of the directory structure
     file_name    = The name of an individual file in the directory
-    
     """
     
+    
     def __init__(self, tenant_id, client_id, client_secret, storage_account, token=None):
-        
+
         super().__init__()
         self.tenant_id = tenant_id
         self.client_id = client_id
@@ -125,8 +125,11 @@ class AzureBlobFileSystem(AbstractFileSystem):
         self.token = token
         self.token_type = None
         self.connect()
-        
+        self.dns_suffix = '.dfs.core.windows.net'
+
     def connect(self):
+        """ Fetch an OAUTh token using a ServicePrincipal """
+        
         url = f"https://login.microsoftonline.com/{self.tenant_id}/oauth2/v2.0/token"
         header = {"Content-Type": "application/x-www-form-urlencoded"}
         data = {"client_id": self.client_id,
@@ -134,29 +137,36 @@ class AzureBlobFileSystem(AbstractFileSystem):
                 "scope": "https://storage.azure.com/.default",
                 "grant_type": "client_credentials"}
         response = requests.post(url=url, headers=header, data=data).json()
-        print(response)
         self.token_type=response['token_type']
         expires_in=response['expires_in']
         ext_expires_in=response['ext_expires_in']
         self.token=response['access_token']
         
-    def create_headers(self):
+    def _make_headers(self):
         headers = {'Content-Type': 'application/x-www-form-urlencoded',
-            'x-ms-version': '2019-02-02',
-                #    'x-ms-date': str(datetime.utcnow()),
-                   'Authorization': f'Bearer {self.token}'}
+                   'x-ms-version': '2019-02-02',
+                   'Authorization': f'Bearer {self.token}'
+                   }
         return headers
-        
-    def create_payload(self):
-        payload = {'resource': 'filesystem',
-                        # 'directory': 'data',
-                        'recursive': 'false'}
-        return payload
     
-    def create_url(self, folder):
-        url=f"https://{self.storage_account}.dfs.core.windows.net/{folder}"
-        return url
+    def _make_base_url(self, filesystem):
+        return f"https://{self.storage_account}{self.dns_suffix}/{filesystem}"
         
+    def ls(self, filesystem: str, resource: str = 'filesystem', recursive: bool = False):
+        """ These are the parameters to be passed via the API call """
+        # We will start by creating the first of each of the verbs
+        
+        url = self._make_base_url(filesystem=filesystem)
+        headers = self._make_headers()
+        
+        payload = {'resource': resource,
+                   'recursive': recursive}
+        
+        response = requests.get(url=url, headers=headers, params=payload)
+        print(url)
+        print(response.url)
+        print(response.json()) 
+
     def make_request(self, url, headers, payload):
         r = requests.get(url=url, headers=headers, params=payload)
         return r
