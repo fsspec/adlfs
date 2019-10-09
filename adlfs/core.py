03e666074e0668a41227a5b5f61269479dc10825
Greg Hayes <gregory.hayes@ecolab.com>
2019-10-09 10:27:30 -0500

    Made notes on future expectation for Gen1 Datalake to delete unneeded state information before passing to workers

--- adlfs/core.py
+++ adlfs/core.py
@@ -150,23 +150,18 @@ class AzureDatalakeFileSystem(AbstractFileSystem):
     def __getstate__(self):
         try:
             dic = self.__dict__.copy()
-            # dic = self.azure_fs.__dict__.copy()
+            # Need to determine what information can be deleted
+            # before passing to the Dask workers
             # del dic['token']
             # del dic['azure']
             logger.debug("Serialize with state: %s", dic)
             return dic
-        except AttributeError:
-            self.do_connect()
-            self.__getstate__()
 
     def __setstate__(self, state):
         try:
             logger.debug("De-serialize with state: %s", state)
-            # self.azure_fs.__dict__.update(state)
             self.__dict__.update(state)
             self.do_connect()
-        except AttributeError:
-            self.__setstate__(state)
 
 
 class AzureDatalakeFile(AzureDLFile):
