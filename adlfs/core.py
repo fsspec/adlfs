c3450051301ba482e8a982d3565e7d39002043fd
Greg Hayes <hayesgb@gmail.com>
2019-11-13 05:22:10 -0600

    Ran black over core.py, setup.py, and tests.py

--- adlfs/core.py
+++ adlfs/core.py
@@ -20,15 +20,14 @@ logger = logging.getLogger(__name__)
 
 
 class AzureDatalakeFileSystem(AbstractFileSystem):
-    
-    
+
     """
     Access Azure Datalake Gen1 as if it were a file system.
 
     This exposes a filesystem-like API on top of Azure Datalake Storage
 
     Examples
-    _________
+    --------
     >>> adl = AzureDatalakeFileSystem(tenant_id="xxxx", client_id="xxxx", 
                                     client_secret="xxxx"
                                     )
@@ -84,70 +83,77 @@ class AzureDatalakeFileSystem(AbstractFileSystem):
         """ Get the store_name from the urlpath and pass to storage_options """
         ops = infer_storage_options(paths)
         out = {}
-        if ops.get('host', None):
-            out['store_name'] = ops['host']
+        if ops.get("host", None):
+            out["store_name"] = ops["host"]
         return out
 
     @classmethod
     def _strip_protocol(cls, path):
         ops = infer_storage_options(path)
-        return ops['path']
-    
+        return ops["path"]
+
     def do_connect(self):
         """Establish connection object."""
-        token = lib.auth(tenant_id=self.tenant_id,
-                        client_id=self.client_id,
-                        client_secret=self.client_secret,
-                        )
-        self.azure_fs = AzureDLFileSystem(token=token,
-                                   store_name=self.store_name)
-        
+        token = lib.auth(
+            tenant_id=self.tenant_id,
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+        )
+        self.azure_fs = AzureDLFileSystem(token=token, store_name=self.store_name)
+
     def ls(self, path, detail=False, invalidate_cache=True):
-        return self.azure_fs.ls(path=path, detail=detail,
-                          invalidate_cache=invalidate_cache)
-    
+        return self.azure_fs.ls(
+            path=path, detail=detail, invalidate_cache=invalidate_cache
+        )
+
     def info(self, path, invalidate_cache=True, expected_error_code=404):
-        info = self.azure_fs.info(path=path, invalidate_cache=invalidate_cache, expected_error_code=expected_error_code)
-        info['size'] = info['length']
+        info = self.azure_fs.info(
+            path=path,
+            invalidate_cache=invalidate_cache,
+            expected_error_code=expected_error_code,
+        )
+        info["size"] = info["length"]
         return info
 
     def _trim_filename(self, fn):
         """ Determine what kind of filestore this is and return the path """
         so = infer_storage_options(fn)
-        fileparts = so['path']
+        fileparts = so["path"]
         return fileparts
 
     def glob(self, path, details=False, invalidate_cache=True):
         """For a template path, return matching files"""
         adlpaths = self._trim_filename(path)
-        filepaths = self.azure_fs.glob(adlpaths, details=details, invalidate_cache=invalidate_cache)
+        filepaths = self.azure_fs.glob(
+            adlpaths, details=details, invalidate_cache=invalidate_cache
+        )
         return filepaths
-    
+
     def isdir(self, path):
         """Is this entry directory-like?"""
         try:
-            return self.info(path)['type'].lower() == 'directory'
+            return self.info(path)["type"].lower() == "directory"
         except FileNotFoundError:
             return False
 
     def isfile(self, path):
         """Is this entry file-like?"""
         try:
-            return self.azure_fs.info(path)['type'].lower() == 'file'
+            return self.azure_fs.info(path)["type"].lower() == "file"
         except:
             return False
 
-    def _open(self, path, mode='rb', block_size=None, autocommit=True):
+    def _open(self, path, mode="rb", block_size=None, autocommit=True):
         return AzureDatalakeFile(self, path, mode=mode)
-    
+
     def read_block(self, fn, offset, length, delimiter=None):
         return self.azure_fs.read_block(fn, offset, length, delimiter)
-        
+
     def ukey(self, path):
-        return tokenize(self.info(path)['modificationTime'])
+        return tokenize(self.info(path)["modificationTime"])
 
     def size(self, path):
-        return self.info(path)['length']
+        return self.info(path)["length"]
 
     def __getstate__(self):
         dic = self.__dict__.copy()
@@ -165,13 +171,18 @@ class AzureDatalakeFileSystem(AbstractFileSystem):
 
 
 class AzureDatalakeFile(AzureDLFile):
-    def __init__(self, fs, path, mode='rb', blocksize=2**25, delimiter=None):
-        super().__init__(azure=fs.azure_fs, path=AzureDLPath(path), mode=mode, 
-                         blocksize=blocksize, delimiter=delimiter)
+    def __init__(self, fs, path, mode="rb", blocksize=2 ** 25, delimiter=None):
+        super().__init__(
+            azure=fs.azure_fs,
+            path=AzureDLPath(path),
+            mode=mode,
+            blocksize=blocksize,
+            delimiter=delimiter,
+        )
         self.fs = fs
         self.path = AzureDLPath(path)
         self.mode = mode
-    
+
     def seek(self, loc, whence=0):
         """ Set current file location
 
@@ -207,7 +218,7 @@ class AzureBlobFileSystem(AbstractFileSystem):
     
 
     Examples
-    _________
+    --------
     >>> abfs = AzureBlobFileSystem(account_name="XXXX", account_key="XXXX", container_name="XXXX")
         adl.ls('')
         
@@ -222,10 +233,10 @@ class AzureBlobFileSystem(AbstractFileSystem):
         })
     """
 
-    protocol = 'abfs'
+    protocol = "abfs"
 
     def __init__(self, account_name: str, container_name: str, account_key: str):
-        
+
         """ Access Azure Datalake Gen2 and Azure Storage using Multiprotocol Access
         
         Parameters
@@ -238,28 +249,30 @@ class AzureBlobFileSystem(AbstractFileSystem):
         self.account_key = account_key
         self.container_name = container_name
         self.do_connect()
-    
+
     @staticmethod
     def _get_kwargs_from_urls(paths):
         """ Get the store_name from the urlpath and pass to storage_options """
-        logging.debug('Getting kwargs from urls...')
+        logging.debug("Getting kwargs from urls...")
         ops = infer_storage_options(paths)
         out = {}
-        if ops.get('host', None):
-            out['container_name'] = ops['host']
-        logging.debug(f'kwargs are:  {out}')
+        if ops.get("host", None):
+            out["container_name"] = ops["host"]
+        logging.debug(f"kwargs are:  {out}")
         return out
-    
+
     @classmethod
     def _strip_protocol(cls, path):
         ops = infer_storage_options(path)
-        ops['path'] = ops['path'].lstrip('/')
-        logging.debug(f'_strip_protocol:  {ops}')
-        return ops['path']
-    
+        ops["path"] = ops["path"].lstrip("/")
+        logging.debug(f"_strip_protocol:  {ops}")
+        return ops["path"]
+
     def do_connect(self):
-        self.blob_fs = BlockBlobService(account_name=self.account_name, account_key=self.account_key)
-        
+        self.blob_fs = BlockBlobService(
+            account_name=self.account_name, account_key=self.account_key
+        )
+
     def ls(self, path: str, detail: bool = False, invalidate_cache: bool = True):
         """ Create a list of blob names from a blob container
         
@@ -274,23 +287,23 @@ class AzureBlobFileSystem(AbstractFileSystem):
         blobs = self.blob_fs.list_blobs(container_name=self.container_name, prefix=path)
         if detail is False:
             pathlist = [blob.name for blob in blobs]
-            logging.debug(f'Detail is False.  Returning {pathlist}')
+            logging.debug(f"Detail is False.  Returning {pathlist}")
             return pathlist
         else:
             pathlist = []
             for blob in blobs:
                 data = {}
-                data['name'] = blob.name
-                data['size'] = blob.properties.content_length
-                data['container_name'] = self.container_name
+                data["name"] = blob.name
+                data["size"] = blob.properties.content_length
+                data["container_name"] = self.container_name
                 if blob.properties.content_settings.content_type is not None:
-                    data['type'] = 'file'
-                else: 
-                    data['type'] = 'directory'
+                    data["type"] = "file"
+                else:
+                    data["type"] = "directory"
                 pathlist.append(data)
-            logging.debug(f'Detail is True:  Returning {pathlist}')
+            logging.debug(f"Detail is True:  Returning {pathlist}")
             return pathlist
-            
+
     def info(self, path: str):
         """ Create a dictionary of path attributes 
         
@@ -298,18 +311,19 @@ class AzureBlobFileSystem(AbstractFileSystem):
         ----------
         path:  An Azure Blob
         """
-        blob = self.blob_fs.get_blob_properties(container_name=self.container_name,
-                                                      blob_name=path)
-        info={}
-        info['name'] = path
-        info['size'] = blob.properties.content_length
-        info['container_name'] = self.container_name
+        blob = self.blob_fs.get_blob_properties(
+            container_name=self.container_name, blob_name=path
+        )
+        info = {}
+        info["name"] = path
+        info["size"] = blob.properties.content_length
+        info["container_name"] = self.container_name
         if blob.properties.content_settings.content_type is not None:
-            info['type'] = 'file'
+            info["type"] = "file"
         else:
-            info['type'] = 'directory'
+            info["type"] = "directory"
         return info
-    
+
     def walk(self, path, maxdepth=None, **kwargs):
         """ Return all files belows path
         
@@ -328,7 +342,7 @@ class AzureBlobFileSystem(AbstractFileSystem):
             on link-based file-systems.
         kwargs: passed to ``ls``
         """
-        
+
         logging.debug(f"abfs.walk() for {path}")
         path = self._strip_protocol(path)
         full_dirs = []
@@ -344,31 +358,35 @@ class AzureBlobFileSystem(AbstractFileSystem):
             # each info name must be at least [path]/part , but here
             # we check also for names like [path]/part/
             name = info["name"].rstrip("/")
-            logging.debug(f"Test path with name, path, type, size:  {name}, {path}, {info['type']}, {info['size']}")
-            if info["type"] == "directory" and name != path and info['size'] == 0:
-                logging.debug(f'{name} is a directory')
-                logging.debug(f'compare name and path: {name}, {path}')
+            logging.debug(
+                f"Test path with name, path, type, size:  {name}, {path}, {info['type']}, {info['size']}"
+            )
+            if info["type"] == "directory" and name != path and info["size"] == 0:
+                logging.debug(f"{name} is a directory")
+                logging.debug(f"compare name and path: {name}, {path}")
                 # do not include "self" path
                 full_dirs.append(name)
                 # Need to add this line to handle an oddity in how
                 # Azure Storage returns blob paths from list operations.
                 # Without it, the ParquetDataset operation by pyarrow fails
-                logging.debug(f"Path name:  {name} is a directory.  Evaluate against {path}")
+                logging.debug(
+                    f"Path name:  {name} is a directory.  Evaluate against {path}"
+                )
                 dirs.append(name.rsplit("/", 1)[-1])
-            elif info['type'] == 'directory' and name == path and info['size'] == 0:
-                logging.debug(f'Skipping {name}.  It is the current directory')
+            elif info["type"] == "directory" and name == path and info["size"] == 0:
+                logging.debug(f"Skipping {name}.  It is the current directory")
                 # The Azure Blob Storage SDK returns the path from a list_blobs()
-                # method call.  This creates an inconsistency across dasks's read methods 
-                # Specifically (read_csv(fpath/fname.csv/*.csv), and the fastparquet vs pyarrow 
+                # method call.  This creates an inconsistency across dasks's read methods
+                # Specifically (read_csv(fpath/fname.csv/*.csv), and the fastparquet vs pyarrow
                 # read_parquet(fpath/fname.parquet) implement a glob call.  Adding
                 # this line reconciles those differences
                 continue
-            elif name == path and info['type'] == 'file':
-                logging.debug(f'name == path')
+            elif name == path and info["type"] == "file":
+                logging.debug(f"name == path")
                 # file-like with same name as give path
                 files.append("")
             else:
-                logging.debug(f'{name} is a file')
+                logging.debug(f"{name} is a file")
                 files.append(name.rsplit("/", 1)[-1])
         yield path, dirs, files
 
@@ -377,48 +395,67 @@ class AzureBlobFileSystem(AbstractFileSystem):
                 for res in self.walk(
                     d,
                     maxdepth=(maxdepth - 1) if maxdepth is not None else None,
-                    **kwargs
+                    **kwargs,
                 ):
                     yield res
-    
+
     def isdir(self, path):
         """Is this entry directory-like?"""
         try:
-            return self.info(path)['type'].lower() == 'directory'
+            return self.info(path)["type"].lower() == "directory"
         except FileNotFoundError:
             return False
-        
+
     def isfile(self, path):
         """Is this entry file-like?"""
         try:
-            return self.info(path)['type'].lower() == 'file'
+            return self.info(path)["type"].lower() == "file"
         except:
             return False
-        
-    def _open(self, path, mode='rb', block_size=None, autocommit=True):
+
+    def _open(self, path, mode="rb", block_size=None, autocommit=True):
         """ Open a file on the datalake, or a block blob """
         logging.debug(f"_open:  {path}")
         return AzureBlobFile(fs=self, path=path, mode=mode)
-    
+
 
 class AzureBlobFile(AbstractBufferedFile):
-    ''' File-like operations on Azure Blobs '''
-    
-    def __init__(self, fs, path, mode='rb', autocommit=True, block_size="default", cache_type="bytes"):
-        super().__init__(fs=fs, path=path, mode=mode, block_size=block_size,
-                         autocommit=autocommit, cache_type=cache_type)
+    """ File-like operations on Azure Blobs """
+
+    def __init__(
+        self,
+        fs,
+        path,
+        mode="rb",
+        autocommit=True,
+        block_size="default",
+        cache_type="bytes",
+    ):
+        super().__init__(
+            fs=fs,
+            path=path,
+            mode=mode,
+            block_size=block_size,
+            autocommit=autocommit,
+            cache_type=cache_type,
+        )
         self.fs = fs
         self.path = path
         self.mode = mode
         self.container_name = fs.container_name
         self.autocommit = autocommit
-    
+
     def _fetch_range(self, start, end):
-        blob = self.fs.blob_fs.get_blob_to_bytes(container_name=self.container_name, blob_name=self.path, 
-                                         start_range=start, end_range=end)
+        blob = self.fs.blob_fs.get_blob_to_bytes(
+            container_name=self.container_name,
+            blob_name=self.path,
+            start_range=start,
+            end_range=end,
+        )
         return blob.content
-    
+
     def _upload_chunk(self, final=False):
         data = self.buffer.getvalue()
-        self.fs.blob_fs.create_blob_from_bytes(container_name=self.container_name, blob_name=self.path, 
-                                  blob=data)
+        self.fs.blob_fs.create_blob_from_bytes(
+            container_name=self.container_name, blob_name=self.path, blob=data
+        )
--- setup.py
+++ setup.py
@@ -5,19 +5,22 @@ from setuptools import setup
 from os import path
 
 
-setup(name='adlfs',
-      version='0.1.0',
-      description='Access Azure Datalake Gen1 with fsspec and dask',
-      url='https://github.com/hayesgb/adlfs/',
-      maintainer='Greg Hayes',
-      maintainer_email='hayesgb@gmail.com',
-      license='BSD',
-      keywords=['file-system', 'dask', 'azure'],
-      packages=['adlfs'],
-      long_description=open('README.rst').read() if exists('README.rst') else '',
-      install_requires=['azure-datalake-store',
-                        'fsspec>=0.4.0<1.0',
-                        'azure-storage-blob==2.1.0',
-                        'pyarrow>=0.15.0',
-                        ],
-      zip_safe=False)
+setup(
+    name="adlfs",
+    version="0.1.1",
+    description="Access Azure Datalake Gen1 with fsspec and dask",
+    url="https://github.com/hayesgb/adlfs/",
+    maintainer="Greg Hayes",
+    maintainer_email="hayesgb@gmail.com",
+    license="BSD",
+    keywords=["file-system", "dask", "azure"],
+    packages=["adlfs"],
+    long_description=open("README.md").read() if exists("README.md") else "",
+    install_requires=[
+        "azure-datalake-store",
+        "fsspec>=0.4.0<1.0",
+        "azure-storage-blob==2.1.0",
+        "pyarrow>=0.15.0",
+    ],
+    zip_safe=False,
+)
