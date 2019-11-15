ba53ea0b29f1d835208f83b066096acb43f707b4
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-11-15 16:51:26 -0600

    networking 2

--- adlfs/tests/conftest.py
+++ adlfs/tests/conftest.py
@@ -6,20 +6,30 @@ from azure.storage.blob import BlockBlobService
 URL = "127.0.0.1:10000"
 USERNAME = "devstoreaccount1"
 KEY = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
-
-
 data = b"0123456789"
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--host", action="store", default="127.0.0.1:10000", help="Host running azurite."
+    )
+
+
 @pytest.fixture
-def storage():
+def host(request):
+    print("host:", request.config.getoption("--host"))
+    return request.config.getoption("--host")
+
+
+@pytest.fixture
+def storage(host):
     """
     Create blob using azurite.
     """
     bbs = BlockBlobService(
         account_name=USERNAME,
         account_key=KEY,
-        custom_domain="http://127.0.0.1:10000/devstoreaccount1",
+        custom_domain=f"http://{host}/devstoreaccount1",
     )
     bbs.create_container("data", timeout=1)
 
