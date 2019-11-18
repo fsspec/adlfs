82fd2f3bd9039693b396d05a4835d4aaaa40ed84
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-11-18 06:34:56 -0600

    ci more

--- adlfs/tests/test_core.py
+++ adlfs/tests/test_core.py
@@ -12,7 +12,7 @@ def test_connect(storage):
 @pytest.mark.xfail(reason="buggy implementation")
 def test_ls(storage):
     fs = adlfs.AzureBlobFileSystem(
-        storage.account_name, "data", storage.account_key, is_emulated=True
+        storage.account_name, "data", storage.account_key,
     )
     assert fs.ls("/") == ["root"]
     assert fs.ls("/root/a/") == ["file.txt"]
