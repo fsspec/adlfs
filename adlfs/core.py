bf6a2d02db460774f596c07f21a474d935371227
Albert DeFusco <albert.defusco@me.com>
2019-11-19 15:51:00 -0600

    blockblobservice arguments
    docstring borrowed from azure.storage.blob.BlockBlobService

--- adlfs/core.py
+++ adlfs/core.py
@@ -7,6 +7,10 @@ import logging
 from azure.datalake.store import lib, AzureDLFileSystem
 from azure.datalake.store.core import AzureDLPath, AzureDLFile
 from azure.storage.blob import BlockBlobService
+from azure.storage.common._constants import (
+    SERVICE_HOST_BASE,
+    DEFAULT_PROTOCOL,
+)
 from fsspec import AbstractFileSystem
 from fsspec.spec import AbstractBufferedFile
 from fsspec.utils import infer_storage_options, stringify_path, tokenize
@@ -230,9 +234,46 @@ class AzureBlobFileSystem(AbstractFileSystem):
 
     Parameters
     ----------
-    storage_account:  Name of the Azure Storage Account
-    account_key:  Access key for the Azure Storage account
-    container_name:  Name of the container or filesystem to be accessed (optional)
+    account_name:
+            The storage account name. This is used to authenticate requests
+            signed with an account key and to construct the storage endpoint. It
+            is required unless a connection string is given, or if a custom
+            domain is used with anonymous authentication.
+    account_key:
+            The storage account key. This is used for shared key authentication.
+            If neither account key or sas token is specified, anonymous access
+            will be used.
+    sas_token:
+             A shared access signature token to use to authenticate requests
+             instead of the account key. If account key and sas token are both
+             specified, account key will be used to sign. If neither are
+             specified, anonymous access will be used.
+    is_emulated:
+            Whether to use the emulator. Defaults to False. If specified, will
+            override all other parameters besides connection string and request
+            session.
+    protocol:
+            The protocol to use for requests. Defaults to https.
+    endpoint_suffix:
+            The host base component of the url, minus the account name. Defaults
+            to Azure (core.windows.net). Override this to use the China cloud
+            (core.chinacloudapi.cn).
+    custom_domain:
+            The custom domain to use. This can be set in the Azure Portal. For
+            example, 'www.mydomain.com'.
+    request_session:
+            The session object to use for http requests.
+    connection_string:
+            If specified, this will override all other parameters besides
+            request session. See
+            http://azure.microsoft.com/en-us/documentation/articles/storage-configure-connection-string/
+            for the connection string format.
+    socket_timeout:
+            If specified, this will override the default socket timeout. The timeout specified is in seconds.
+            See DEFAULT_SOCKET_TIMEOUT in _constants.py for the default value.
+    token_credential:
+            A token credential used to authenticate HTTPS requests. The token value
+            should be updated before its expiration.
 
     Examples
     --------
@@ -252,18 +293,32 @@ class AzureBlobFileSystem(AbstractFileSystem):
 
     def __init__(
         self,
-        account_name: str,
         container_name: str,
-        account_key: str,
-        custom_domain: str = None,
-        is_emulated: bool = False,
+        account_name=None,
+        account_key=None,
+        sas_token=None,
+        is_emulated=False,
+        protocol=DEFAULT_PROTOCOL,
+        endpoint_suffix=SERVICE_HOST_BASE,
+        custom_domain=None,
+        request_session=None,
+        connection_string=None,
+        socket_timeout=None,
+        token_credential=None
     ):
         AbstractFileSystem.__init__(self)
+        self.container_name = container_name
         self.account_name = account_name
         self.account_key = account_key
-        self.container_name = container_name
-        self.custom_domain = custom_domain
+        self.sas_token = sas_token
         self.is_emulated = is_emulated
+        self.protocol = protocol
+        self.endpoint_suffix = endpoint_suffix
+        self.custom_domain = custom_domain
+        self.request_session = request_session
+        self.connection_string = connection_string
+        self.socket_timeout = socket_timeout
+        self.token_credential = token_credential
         self.do_connect()
 
     @staticmethod
@@ -286,10 +341,17 @@ class AzureBlobFileSystem(AbstractFileSystem):
 
     def do_connect(self):
         self.blob_fs = BlockBlobService(
-            account_name=self.account_name,
-            account_key=self.account_key,
-            custom_domain=self.custom_domain,
-            is_emulated=self.is_emulated,
+            account_name = self.account_name,
+            account_key = self.account_key,
+            sas_token = self.sas_token,
+            is_emulated = self.is_emulated,
+            protocol = self.protocol,
+            endpoint_suffix = self.endpoint_suffix,
+            custom_domain = self.custom_domain,
+            request_session = self.request_session,
+            connection_string = self.connection_string,
+            socket_timeout = self.socket_timeout,
+            token_credential = self.token_credential
         )
 
     def ls(
