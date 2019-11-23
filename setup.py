5302483f26fb7c646dbf68a9551bb964434de753
Albert DeFusco <albert.defusco@me.com>
2019-11-23 10:21:22 -0600

    versioneer and tests in conda.recipe

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@
 
 from os.path import exists
 from setuptools import setup
+import versioneer
 
 
 setup(
     name="adlfs",
-    version="0.1.2",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description="Access Azure Datalake Gen1 with fsspec and dask",
     url="https://github.com/hayesgb/adlfs/",
     maintainer="Greg Hayes",
@@ -17,10 +19,9 @@ setup(
     long_description_content_type="text/markdown",
     long_description=open("README.md").read() if exists("README.md") else "",
     install_requires=[
-        "azure-datalake-store",
-        "fsspec>=0.4.0,<0.6",
-        "azure-storage-blob==2.1.0",
-        "pyarrow>=0.15.0",
+        "azure-datalake-store>=0.0.46,<0.1",
+        "azure-storage-blob>=2.1.0,<3.0.0",
+        "fsspec>=0.6.0,<1.0"
     ],
     zip_safe=False,
 )
