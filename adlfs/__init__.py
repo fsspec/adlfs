4ab19b5eed5373cc209cbbd910e1e715a0b2b91d
Greg Hayes <hayesgb@gmail.com>
2019-12-16 19:20:04 -0600

    removed old versioneer files

--- adlfs/__init__.py
+++ adlfs/__init__.py
@@ -1,12 +1,4 @@
 from .core import AzureDatalakeFileSystem
 from .core import AzureBlobFileSystem, AzureBlobFile
-from ._version import get_versions
 
 __all__ = ["AzureBlobFileSystem", "AzureBlobFile", "AzureDatalakeFileSystem"]
-
-__version__ = get_versions()["version"]
-del get_versions
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
