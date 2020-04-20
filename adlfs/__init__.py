bf4abbbfe8e39ee7b3ba5de49f014ad0aa10250f
Greg Hayes <hayesgb@gmail.com>
2020-04-20 16:43:12 -0500

    Updated fsspec requirement in azure-pipelines

--- adlfs/__init__.py
+++ adlfs/__init__.py
@@ -6,3 +6,7 @@ __all__ = ["AzureBlobFileSystem", "AzureBlobFile", "AzureDatalakeFileSystem"]
 
 __version__ = get_versions()["version"]
 del get_versions
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
