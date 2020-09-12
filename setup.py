db9dd7900e4a0a5291613e38d175af9d4d3c7302
Greg Hayes <hayesgb@gmail.com>
2020-09-12 08:27:45 -0500

    Updated setup.py to use requirements.txt during installation

--- setup.py
+++ setup.py
@@ -5,6 +5,20 @@ from setuptools import setup
 import versioneer
 
 
+def is_ignored(line):
+    line = line.strip()
+    return (not line) or (line[0] == "#")
+
+def load_deps(path):
+    """Load dependencies from requirements file"""
+    with open(path) as fp:
+        return [line.strip() for line in fp if not is_ignored(line)]
+
+with open("README.md") as fp:
+    long_desc = fp.read()
+
+install_requires = list(load_deps("requirements.txt"))
+
 setup(
     name="adlfs",
     version=versioneer.get_version(),
@@ -18,15 +32,7 @@ setup(
     packages=["adlfs"],
     long_description_content_type="text/markdown",
     long_description=open("README.md").read() if exists("README.md") else "",
-    install_requires=[
-        "aiohttp",
-        "azure-core>=1.5.0",
-        "azure-datalake-store>=0.0.46,<0.1",
-        "azure-identity",
-        "azure-storage-blob>=12.5.0",
-        "fsspec>=0.8.0",
-        "msrestazure",
-    ],
+    install_requires=install_requires,
     tests_require=["pytest>5.0,<6.0", "docker"],
     zip_safe=False,
 )
