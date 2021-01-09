eba78730d70681da91de997b01b306e3df813f99
Greg Hayes <hayesgb@gmail.com>
2021-01-09 07:04:57 -0600

    Added AbstractBufferedFile

--- adlfs/spec.py
+++ adlfs/spec.py
@@ -25,6 +25,7 @@ from fsspec.asyn import (
     get_loop,
     sync_wrapper,
 )
+from fsspec.spec import AbstractBufferedFile
 from fsspec.utils import infer_storage_options, tokenize
 from .utils import filter_blobs
 
@@ -1403,7 +1404,7 @@ class AzureBlobFileSystem(AsyncFileSystem):
         )
 
 
-class AzureBlobFile(io.IOBase):
+class AzureBlobFile(AbstractBufferedFile):
     """ File-like operations on Azure Blobs """
 
     DEFAULT_BLOCK_SIZE = 5 * 2 ** 20
@@ -1415,7 +1416,7 @@ class AzureBlobFile(io.IOBase):
         mode: str = "rb",
         block_size="default",
         autocommit: bool = True,
-        cache_type: str = "readahead",
+        cache_type: str = "bytes",
         cache_options: dict = {},
         metadata=None,
         **kwargs,
@@ -1542,61 +1543,6 @@ class AzureBlobFile(io.IOBase):
                 f"Unable to fetch container_client with provided params for {e}!!"
             )
 
-    @property
-    def closed(self):
-        # get around this attr being read-only in IOBase
-        # use getattr here, since this can be called during del
-        return getattr(self, "_closed", True)
-
-    @closed.setter
-    def closed(self, c):
-        self._closed = c
-
-    def __hash__(self):
-        if "w" in self.mode:
-            return id(self)
-        else:
-            return int(tokenize(self.details), 16)
-
-    def __eq__(self, other):
-        """Files are equal if they have the same checksum, only in read mode"""
-        return self.mode == "rb" and other.mode == "rb" and hash(self) == hash(other)
-
-    def commit(self):
-        """Move from temp to final destination"""
-
-    def tell(self):
-        """ Current file location """
-        return self.loc
-
-    def seek(self, loc, whence=0):
-        """Set current file location
-        Parameters
-        ----------
-        loc: int
-            byte location
-        whence: {0, 1, 2}
-            from start of file, current location or end of file, resp.
-        """
-        loc = int(loc)
-        if not self.mode == "rb":
-            raise OSError("Seek only available in read mode")
-        if whence == 0:
-            nloc = loc
-        elif whence == 1:
-            nloc = self.loc + loc
-        elif whence == 2:
-            nloc = self.size + loc
-        else:
-            raise ValueError("invalid whence (%s, should be 0, 1 or 2)" % whence)
-        if nloc < 0:
-            raise ValueError("Seek before start of file")
-        self.loc = nloc
-        return self.loc
-
-    def discard(self):
-        """Throw away temporary file"""
-
     def _fetch_range(self, start: int, end: int, **kwargs):
         """
         Download a chunk of data specified by start and end
@@ -1611,7 +1557,8 @@ class AzureBlobFile(io.IOBase):
         blob = self.container_client.download_blob(
             blob=self.blob, offset=start, length=end
         )
-        return blob.readall()
+        # return blob.readall()
+        return blob.readinto()
 
     def __initiate_upload(self, **kwargs):
         pass
@@ -1685,187 +1632,7 @@ class AzureBlobFile(io.IOBase):
                 blob_type=BlobType.AppendBlob,
                 metadata=self.metadata,
             )
-
-    def flush(self, force=False):
-        """
-        Write buffered data to backend store.
-        Writes the current buffer, if it is larger than the block-size, or if
-        the file is being closed.
-        Parameters
-        ----------
-        force: bool
-            When closing, write the last block even if it is smaller than
-            blocks are allowed to be. Disallows further writing to this file.
-        """
-
-        if self.closed:
-            raise ValueError("Flush on closed file")
-        if force and self.forced:
-            raise ValueError("Force flush cannot be called more than once")
-        if force:
-            self.forced = True
-        if self.mode not in {"wb", "ab"}:
-            # no-op to flush on read-mode
-            return
-
-        if not force and self.buffer.tell() < self.blocksize:
-            # Defer write on small block
-            return
-
-        if self.offset is None:
-            # Initialize a multipart upload
-            self.offset = 0
-            self._initiate_upload()
-
-        if self._upload_chunk(final=force) is not False:
-            self.offset += self.buffer.seek(0, 2)
-            self.buffer = io.BytesIO()
-
-    def write(self, data):
-        """
-        Write data to buffer.
-        Buffer only sent on flush() or if buffer is greater than
-        or equal to blocksize.
-        Parameters
-        ----------
-        data: bytes
-            Set of bytes to be written.
-        """
-
-        if self.mode not in {"wb", "ab"}:
-            raise ValueError("File not in write mode")
-        if self.closed:
-            raise ValueError("I/O operation on closed file.")
-        if self.forced:
-            raise ValueError("This file has been force-flushed, can only close")
-        out = self.buffer.write(data)
-        self.loc += out
-        if self.buffer.tell() >= self.blocksize:
-            self.flush()
-        return out
-
-    def readuntil(self, char=b"\n", blocks=None):
-        """Return data between current position and first occurrence of char
-        char is included in the output, except if the end of the tile is
-        encountered first.
-        Parameters
-        ----------
-        char: bytes
-            Thing to find
-        blocks: None or int
-            How much to read in each go. Defaults to file blocksize - which may
-            mean a new read on every call.
-        """
-        out = []
-        while True:
-            start = self.tell()
-            part = self.read(blocks or self.blocksize)
-            if len(part) == 0:
-                break
-            found = part.find(char)
-            if found > -1:
-                out.append(part[: found + len(char)])
-                self.seek(start + found + len(char))
-                break
-            out.append(part)
-        return b"".join(out)
-
-    def readline(self):
-        """Read until first occurrence of newline character
-        Note that, because of character encoding, this is not necessarily a
-        true line ending.
-        """
-        return self.readuntil(b"\n")
-
-    def __next__(self):
-        out = self.readline()
-        if out:
-            return out
-        raise StopIteration
-
-    def __iter__(self):
-        return self
-
-    def readlines(self):
-        """Return all data, split by the newline character"""
-        data = self.read()
-        lines = data.split(b"\n")
-        out = [l + b"\n" for l in lines[:-1]]
-        if data.endswith(b"\n"):
-            return out
         else:
-            return out + [lines[-1]]
-        # return list(self)  ???
-
-    def readinto1(self, b):
-        return self.readinto(b)
-
-    def readinto(self, b):
-        """mirrors builtin file's readinto method
-        https://docs.python.org/3/library/io.html#io.RawIOBase.readinto
-        """
-        data = self.read(len(b))
-        memoryview(b).cast("B")[: len(data)] = data
-        return len(data)
-
-    def read(self, length=-1):
-        """
-        Return data from cache, or fetch pieces as necessary
-        Parameters
-        ----------
-        length: int (-1)
-            Number of bytes to read; if <0, all remaining bytes.
-        """
-
-        length = -1 if length is None else int(length)
-        if self.mode != "rb":
-            raise ValueError("File not in read mode")
-        if length < 0:
-            length = self.size - self.loc
-        if self.closed:
-            raise ValueError("I/O operation on closed file.")
-        logger.debug("%s read: %i - %i" % (self, self.loc, self.loc + length))
-        if length == 0:
-            # don't even bother calling fetch
-            return b""
-        out = self.cache._fetch(self.loc, self.loc + length)
-        self.loc += len(out)
-        return out
-
-    def close(self):
-        """Close file
-        Finalizes writes, discards cache
-        """
-        if self.closed:
-            return
-        if self.mode == "rb":
-            self.cache = None
-        else:
-            if not self.forced:
-                self.flush(force=True)
-            if self.fs is not None:
-                self.fs.invalidate_cache(self.path)
-                self.fs.invalidate_cache(self.fs._parent(self.path))
-
-        self.closed = True
-
-    def readable(self):
-        """Whether opened for reading"""
-        return self.mode == "rb" and not self.closed
-
-    def seekable(self):
-        """Whether is seekable (only in read mode)"""
-        return self.readable()
-
-    def writable(self):
-        """Whether opened for writing"""
-        return self.mode in {"wb", "ab"} and not self.closed
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *args):
-        self.close()
-
-    def __del__(self):
-        self.close()
+            raise ValueError(
+                "File operation modes other than wb or ab are not yet supported for upload_chunk"
+            )
