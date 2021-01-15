91b37e79cce2cef7c28a4c5432b4aa62e6f4f757
Ray Bell <rayjohnbell0@gmail.com>
2021-01-15 15:51:48 -0500

    rm 3.6 from classifiers

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
