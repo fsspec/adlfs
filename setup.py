6e86191cf7b652a1c87e4709c69e932e887bc857
Batuhan Taskaya <isidentical@gmail.com>
2021-04-08 10:36:06 +0300

    remove upperbound limits
    Co-authored-by: Tom Augspurger <tom.augspurger88@gmail.com>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "azure-datalake-store>=0.0.46,<0.1",
         "azure-identity",
         "azure-storage-blob>=12.5.0,<12.7.0",
-        "fsspec>=0.9.0,<0.10.0",
+        "fsspec>=0.9.0",
         "msrestazure",
         "requests>=2.22.0,<3.0",
     ],
