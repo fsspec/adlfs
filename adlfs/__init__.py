009c265b2960b081c6d9ffb20a1123e4d8178f15
Manjur Ansari <manjuransari143@gmail.com>
2021-09-29 06:26:36 +0530

    Added entrypoint to handle abfss support (#278)
    * Updated _strip_protocol method to handle abfss protocol
    
    * typo fix
    
    * Added entrypoint to support abfss
    
    * Added new line
    
    * fix lint error
    
    Co-authored-by: Manjur Ansari <maansar@microsoft.com>

--- adlfs/__init__.py
+++ adlfs/__init__.py
@@ -2,8 +2,6 @@ from .spec import AzureDatalakeFileSystem
 from .spec import AzureBlobFileSystem, AzureBlobFile
 from ._version import get_versions
 
-import fsspec
-
 __all__ = ["AzureBlobFileSystem", "AzureBlobFile", "AzureDatalakeFileSystem"]
 
 __version__ = get_versions()["version"]
@@ -13,17 +11,3 @@ from ._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
-
-if hasattr(fsspec, "register_implementation"):
-    fsspec.register_implementation("abfss", AzureBlobFileSystem, clobber=True)
-else:
-    from fsspec.registry import known_implementations
-
-    known_implementations["abfss"] = {
-        "class": "adlfs.AzureBlobFileSystem",
-        "err": "Please install adlfs to use the abfss protocol",
-    }
-
-    del known_implementations
-
-del fsspec  # clear the module namespace
