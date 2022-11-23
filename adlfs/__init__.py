f17345279a26e2b820ca5f4e8d72a9d3ccd910fb
Greg Hayes <hayesgb@gmail.com>
2022-11-23 17:27:23 -0600

    Fixed linting

--- adlfs/__init__.py
+++ adlfs/__init__.py
@@ -1,6 +1,7 @@
 from ._version import get_versions
-from .spec import AzureBlobFile, AzureBlobFileSystem
 from .gen1 import AzureDatalakeFileSystem
+from .spec import AzureBlobFile, AzureBlobFileSystem
+
 __all__ = ["AzureBlobFileSystem", "AzureBlobFile", "AzureDatalakeFileSystem"]
 
 __version__ = get_versions()["version"]
