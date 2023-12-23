7faaf3ec96e28c029a311950d22c0dc22a48c148
Ruslan Kuprieiev <kupruser@gmail.com>
2023-12-23 08:10:45 +0200

    adlfs: fix version typo (#449)
    Per https://github.com/fsspec/adlfs/pull/446#discussion_r1435453899

--- adlfs/__init__.py
+++ adlfs/__init__.py
@@ -4,8 +4,8 @@ from .spec import AzureBlobFile, AzureBlobFileSystem
 __all__ = ["AzureBlobFileSystem", "AzureBlobFile", "AzureDatalakeFileSystem"]
 
 try:
-    from ._dvc_version import version as __version__  # type: ignore[import]
-    from ._dvc_version import version_tuple  # type: ignore[import]
+    from ._version import version as __version__  # type: ignore[import]
+    from ._version import version_tuple  # type: ignore[import]
 except ImportError:
     __version__ = "UNKNOWN"
     version_tuple = (0, 0, __version__)  # type: ignore[assignment]
