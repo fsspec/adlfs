d8d1d754a7d157b4c1ff3cf053329fd455588437
dorbaker <dorbaker@microsoft.com>
2024-06-24 09:36:50 -0400

    Formatting fixes.

--- adlfs/spec.py
+++ adlfs/spec.py
@@ -135,11 +135,6 @@ class AzureBlobFileSystem(AsyncFileSystem):
         The storage account key. This is used for shared key authentication.
         If any of account key, sas token or client_id is specified, anonymous access
         will be used.
-    account_host: str
-        The storage account host. This string is the entire url to the for the storage
-        after the https://, i.e. "https://{account_host}". This parameter is only
-        required for Azure clouds where account urls do not end with "blob.core.windows.net". 
-        Note that the account_name parameter is still required.
     sas_token: str
         A shared access signature token to use to authenticate requests
         instead of the account key. If account key and sas token are both
@@ -195,6 +190,11 @@ class AzureBlobFileSystem(AsyncFileSystem):
     read_timeout: int
         The number of seconds the client will wait, between consecutive read operations,
         for a response from the server while uploading or downloading a blob.
+    account_host: str
+        The storage account host. This string is the entire url to the for the storage
+        after the https://, i.e. "https://{account_host}". This parameter is only
+        required for Azure clouds where account urls do not end with "blob.core.windows.net".
+        Note that the account_name parameter is still required.
 
     Pass on to fsspec:
 
@@ -240,7 +240,6 @@ class AzureBlobFileSystem(AsyncFileSystem):
     def __init__(
         self,
         account_name: str = None,
-        account_host: str = None,
         account_key: str = None,
         connection_string: str = None,
         credential: str = None,
@@ -263,6 +262,7 @@ class AzureBlobFileSystem(AsyncFileSystem):
         timeout: Optional[int] = None,
         connection_timeout: Optional[int] = None,
         read_timeout: Optional[int] = None,
+        account_host: str = None,
         **kwargs,
     ):
         super_kwargs = {
@@ -293,7 +293,8 @@ class AzureBlobFileSystem(AsyncFileSystem):
             ]
         self.location_mode = location_mode
         self.credential = credential
-        if account_host: self.account_host = account_host
+        if account_host:
+            self.account_host = account_host
         self.request_session = request_session
         self.assume_container_exists = assume_container_exists
         if socket_timeout is not _SOCKET_TIMEOUT_DEFAULT:
