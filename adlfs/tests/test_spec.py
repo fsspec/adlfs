82cde827f41094e20a064b76a4ec71bdb0f72f04
Martin Durant <martindurant@users.noreply.github.com>
2024-12-13 21:11:14 -0500

    Implement exclusive mode (#491)

--- adlfs/tests/test_spec.py
+++ adlfs/tests/test_spec.py
@@ -5,9 +5,11 @@ from unittest import mock
 
 import azure.storage.blob.aio
 import dask.dataframe as dd
+import fsspec
 import numpy as np
 import pandas as pd
 import pytest
+from packaging.version import parse as parse_version
 from pandas.testing import assert_frame_equal
 
 from adlfs import AzureBlobFile, AzureBlobFileSystem
@@ -1982,3 +1984,64 @@ def test_hdi_isfolder_case(storage: azure.storage.blob.BlobServiceClient, key: s
 
     result = fs.info("data/folder")
     assert result["type"] == "directory"
+
+
+def test_pipe_file_x(storage: azure.storage.blob.BlobServiceClient):
+    fs = AzureBlobFileSystem(
+        account_name=storage.account_name,
+        connection_string=CONN_STR,
+        skip_instance_cache=True,
+        timeout=11,
+        connection_timeout=12,
+        read_timeout=13,
+    )
+    fs.pipe_file("data/afile", b"data", mode="create")  # ok
+    fs.pipe_file("data/afile", b"data2")  # ok
+    with pytest.raises(FileExistsError):
+        fs.pipe_file("data/afile", b"data3", mode="create")
+    assert fs.cat_file("data/afile") == b"data2"
+
+
+def test_put_file_x(storage: azure.storage.blob.BlobServiceClient, tmpdir):
+    fs = AzureBlobFileSystem(
+        account_name=storage.account_name,
+        connection_string=CONN_STR,
+        skip_instance_cache=True,
+        timeout=11,
+        connection_timeout=12,
+        read_timeout=13,
+    )
+    fn = f"{tmpdir}/afile"
+    with open(fn, "wb") as f:
+        f.write(b"data")
+    fs.put_file(fn, "data/afile", mode="create")  # ok
+    fs.put_file(fn, "data/afile")  # ok
+    with pytest.raises(FileExistsError):
+        fs.put_file(fn, "data/afile", mode="create")
+
+    assert fs.cat_file("data/afile") == b"data"
+
+
+@pytest.mark.xfail(
+    parse_version(fsspec.__version__) <= parse_version("2024.10.0"),
+    reason="not supported upstream yet",
+)
+def test_open_file_x(storage: azure.storage.blob.BlobServiceClient, tmpdir):
+    fs = AzureBlobFileSystem(
+        account_name=storage.account_name,
+        connection_string=CONN_STR,
+        skip_instance_cache=True,
+        timeout=11,
+        connection_timeout=12,
+        read_timeout=13,
+    )
+    with fs.open("data/afile", "xb") as f:
+        f.write(b"data")
+    with pytest.raises(FileExistsError):
+        with fs.open("data/afile", "xb") as f:
+            f.write(b"data2")
+    with pytest.raises(FileExistsError):
+        # zero-length file has a different codepath
+        with fs.open("data/afile", "xb") as f:
+            pass
+    assert fs.cat_file("data/afile") == b"data"
