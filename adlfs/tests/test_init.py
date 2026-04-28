0850f9423c399870a5d5ee21b6f1dee7e9316174
Anjali Ratnam <anjaliratnam@microsoft.com>
2026-04-28 09:13:45 -0700

    linting fixes

--- adlfs/tests/test_init.py
+++ adlfs/tests/test_init.py
@@ -1,26 +1,40 @@
-import pytest
 import fsspec
-from adlfs import AzureBlobFileSystem, AzureBlobFile
+import pytest
+
+from adlfs import AzureBlobFile, AzureBlobFileSystem
 
 
 def test_adl_protocol_attribute_error():
-    with pytest.raises(AttributeError, match="Gen1 has been retired since February 29, 2024 and "
-            "is no longer supported by adlfs"):
+    with pytest.raises(
+        AttributeError,
+        match="Gen1 has been retired since February 29, 2024 and "
+        "is no longer supported by adlfs",
+    ):
         fsspec.filesystem("adl")
 
 
 def test_gen1_class_attribute_error():
     import adlfs
-    with pytest.raises(AttributeError, match="Gen1 has been retired since February 29, 2024 and "
-            "is no longer supported by adlfs"):
+
+    with pytest.raises(
+        AttributeError,
+        match="Gen1 has been retired since February 29, 2024 and "
+        "is no longer supported by adlfs",
+    ):
         adlfs.AzureDatalakeFileSystem
+
 
 def test_known_attributes():
     import adlfs
+
     assert adlfs.AzureBlobFileSystem is AzureBlobFileSystem
     assert adlfs.AzureBlobFile is AzureBlobFile
 
+
 def test_getattr_unknown_attribute():
     import adlfs
-    with pytest.raises(AttributeError, match="module 'adlfs' has no attribute 'unknown_attribute'"):
+
+    with pytest.raises(
+        AttributeError, match="module 'adlfs' has no attribute 'unknown_attribute'"
+    ):
         getattr(adlfs, "unknown_attribute")
