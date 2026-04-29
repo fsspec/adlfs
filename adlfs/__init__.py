74ed2be8fc02cd2216ca1e6cc04d2fed98ddc017
anjaliratnam-msft <anjaliratnam@microsoft.com>
2026-04-29 10:09:55 -0700

    Removed ADLS Gen1 Support (#545)

--- adlfs/__init__.py
+++ adlfs/__init__.py
@@ -1,5 +1,17 @@
-from .gen1 import AzureDatalakeFileSystem
 from .spec import AzureBlobFile, AzureBlobFileSystem
 from .utils import __version__, version_tuple  # noqa: F401
 
-__all__ = ["AzureBlobFileSystem", "AzureBlobFile", "AzureDatalakeFileSystem"]
+__all__ = ["AzureBlobFileSystem", "AzureBlobFile"]
+
+
+def __getattr__(name):
+    msg = f"module {__name__!r} has no attribute {name!r}"
+    if name == "AzureDatalakeFileSystem":
+        err = (
+            "Azure Data Lake Storage (ADLS) Gen1 has been retired since February 29, 2024 and "
+            "is no longer supported by adlfs. It is recommended to use the az:// protocol "
+            "and/or adlfs.AzureBlobFileSystem class which support Azure Blob Storage and Azure "
+            "Data Lake Storage Gen2. "
+        )
+        msg = err + msg
+    raise AttributeError(msg)
